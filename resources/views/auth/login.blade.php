c50cc5334d887a8063f4625a2efb15106309e455
ALealM <alfredoleal.029@gmail.com>
2017-10-18 17:44:10 -0500

    Add files via upload

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1127,7 +1127,7 @@ div {
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">8Microrregión Huasteca Suroeste</h4>
+                <h4 class="modal-title">Microrregión Huasteca Suroeste</h4>
             </div>
             <div class="modal-body">
                 <!----------->
